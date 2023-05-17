3d31d7486701a20fa5fd03e5c9cca640fd7eb5d5
AusraO <ausra.241@protonmail.com>
2023-05-17 11:23:35 +0300

    added provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { UsersProvider } from './contexts/UsersContext';
 import { NewUsersProvider } from './contexts/NewUsersContext';
+import { PostsProvider } from './contexts/PostsContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <PostsProvider>
     <NewUsersProvider>
  <UsersProvider>
     <App />
     </UsersProvider>
     </NewUsersProvider>
+    </PostsProvider>
 );
